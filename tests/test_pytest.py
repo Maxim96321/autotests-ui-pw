28fcb80cba95f0e64bd358324e49f09df40db61e
Maxim96321 <maxim96321@mail.ru>
2026-02-09 13:50:32 +0400

    6.1 ведение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,18 @@
+def test_user_login1():
+    print('Hello 1')
+
+
+def test_user_login2():
+    print('Hello 2')
+
+
+def test_user_login3():
+    print('Hello 3')
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+
+def test_assert_nagative_case():
+    assert (2 + 2) == 5
