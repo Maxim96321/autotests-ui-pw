ed5b208b5105c98b6691a8a9b61d473269809c5f
Maxim96321 <maxim96321@mail.ru>
2026-02-18 14:14:01 +0400

    7.2 рактикуемся в реализации PageObject 1 видео

---
+++ pages/courses_list_page.py
@@ -0,0 +1,33 @@
+from pages.base_page import BasePage
+from playwright.sync_api import Page, expect
+
+
+class CoursesListPage(BasePage):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        self.courses_title = page.get_by_test_id("courses-list-toolbar-title-text")
+        self.create_course_button = page.get_by_test_id("courses-list-toolbar-create-course-button")
+
+        self.empty_view_icon = page.get_by_test_id("courses-list-empty-view-icon")
+        self.empty_view_title = page.get_by_test_id("courses-list-empty-view-title-text")
+        self.empty_view_description = page.get_by_test_id("courses-list-empty-view-description-text")
+
+    def check_visible_courses_title(self):
+        expect(self.courses_title).to_be_visible()
+        expect(self.courses_title).to_have_text("Courses")
+
+    def check_visible_empty_view(self):
+        expect(self.empty_view_icon).to_be_visible()
+
+        expect(self.empty_view_title).to_be_visible()
+        expect(self.empty_view_title).to_have_text("There is no results")
+
+        expect(self.empty_view_description).to_be_visible()
+        expect(self.empty_view_description).to_have_text("Results from the load test pipeline will be displayed here")
+
+    def check_create_course_button(self):
+        expect(self.create_course_button).to_be_visible()
+
+    def click_create_course_button(self):
+        expect(self.create_course_button).to_be_visible()
