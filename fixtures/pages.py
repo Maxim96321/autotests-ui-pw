b8c88c73e55980b695511a8e0a9618fd95f85496
Maxim96321 <maxim96321@mail.ru>
2026-02-26 14:00:37 +0400

    7.3 накомство с PageComponent домашнее задани

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -34,5 +34,10 @@ def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
 
 
 @pytest.fixture()
+def courses_list_page_with_state(chromium_page_with_state: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page_with_state)
+
+
+@pytest.fixture()
 def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
     return CreateCoursePage(page=chromium_page_with_state)
