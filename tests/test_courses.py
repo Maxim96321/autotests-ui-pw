f709de03503acf37479d4f84e1942c5d4fbe130b
Maxim96321 <maxim96321@mail.ru>
2026-03-03 14:09:59 +0400

    7.4 рактикуемся в реализации PageComponent 4 и 5  видео

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -22,15 +22,14 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page):
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form(title="", max_score="0", min_score="0", description="",
                                                         estimated_time="")
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
-    create_course_page.upload_preview_image("C:/Cours Playwrite/autotest-ui-pw/testdata/files/IMG_4866.png")
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.image_upload_widget.upload_preview_image("C:/Cours Playwrite/autotest-ui-pw/testdata/files/IMG_4866.png")
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(title="Playwright",
                                                estimated_time="2 weeks",
                                                description="Playwright",
